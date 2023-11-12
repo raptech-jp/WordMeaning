66eaf08a8376c320fc2673716704bbcec56a5702
raptech-jp <paparukisan@gmail.com>
2023-11-12 21:37:28 +0900

    [update]Improved UX

--- db.py
+++ db.py
@@ -1,54 +1,76 @@
 import os
-import gpt
+import sys
 import mysql.connector
+from gpt import Word
 
-conn = mysql.connector.connect(
-    host='localhost',
-    user=os.getenv("USER"),
-    password=os.getenv("PASSWORD"),
-    database=os.getenv("DATABASE"),
-    port='3306',
-    charset='utf8'
-)
+def create_database_connection():
+    return mysql.connector.connect(
+        host='localhost',
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+        port='3306',
+        charset='utf8'
+    )
 
-cur = conn.cursor()
-
-name = input("Your name: ")
-
-cur.execute("SHOW TABLES LIKE 'word'")
-table_exists = cur.fetchone()
-
-if not table_exists:
-    cur.execute("""
-        CREATE TABLE word (
+def create_word_table(cursor):
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS word (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) ,
+            name VARCHAR(255),
             word VARCHAR(255) NOT NULL,
             meaning TEXT NOT NULL,
             example_english_sentence TEXT NOT NULL,
             part_of_speech TEXT NOT NULL
         )
     """)
-    conn.commit()
 
-word = input("Enter a word: ")
-translated = gpt.Word(word)
+def table_exists(cursor):
+    cursor.execute("SHOW TABLES LIKE 'word'")
+    return cursor.fetchone() is not None
 
-cur.execute("SELECT word FROM word WHERE word=%s", (translated.get_word(),))
-existing_word = cur.fetchone()
+def insert_word(cursor, translated, name):
+    cursor.execute("SELECT word FROM word WHERE word=%s", (translated.get_word(),))
+    existing_word = cursor.fetchone()
 
-if not existing_word:
-    cur.execute("INSERT INTO word (name, word, meaning, example_english_sentence, part_of_speech) VALUES (%s,%s, %s, %s, %s)",
-                (name, translated.get_word(), translated.get_meaning(), translated.get_example_english_sentence(), translated.get_part_of_speech()))
-    conn.commit()
-else:
-    print(f"The word '{translated.get_word()}' already exists in the database.")
+    if not existing_word:
+        cursor.execute("""
+            INSERT INTO word (name, word, meaning, example_english_sentence, part_of_speech)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (name, translated.get_word(), translated.get_meaning(), translated.get_example_english_sentence(), translated.get_part_of_speech()))
+    else:
+        print(f"The word '{translated.get_word()}' already exists in the database.")
 
-print(translated.get_word())
+def main():
+    try:
+        name = input("Your name: ")
+        conn = create_database_connection()
+        cur = conn.cursor()
 
-cur.execute("SELECT word, meaning, example_english_sentence, part_of_speech FROM word where name=%s", (name,))
-items_list = cur.fetchall()
-for fr in items_list:
-    print(fr)
+        if not table_exists(cur):
+            create_word_table(cur)
+            conn.commit()
 
-conn.close()
+        while True:
+            word = input("\nEnter a word (press Ctrl+C to exit): ")
+
+            if not word:
+                continue
+
+            translated = Word(word)
+            insert_word(cur, translated, name)
+
+            cur.execute("SELECT word, meaning, example_english_sentence, part_of_speech FROM word WHERE name=%s", (name,))
+            cur.fetchall()
+            conn.commit()
+
+    except KeyboardInterrupt:
+        print("Program terminated.")
+        sys.exit()
+
+    finally:
+        cur.close()
+        conn.close()
+
+if __name__ == "__main__":
+    main()
